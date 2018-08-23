3ccb742338eaaa8237517601acb5b8b1419f3053
firecoded <yepyepguru@gmail.com>
2018-08-23 12:58:24 -0700

    still in progress, list data

---
+++ src/components/list.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class List extends Component {
+	render() {
+		return (
+			<ul className = 'collection'>
+				<li className = 'collection-item'>collection</li>
+			</ul>
+		);
+	}
+}
+
+export default List;
